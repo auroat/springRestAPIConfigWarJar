970d13dd8e2e0844f69aa62a8009b4c347e43d28
Aurel Pintea <winofame@gmail.com>
2018-09-22 12:57:52 +0200

    Spring BOOT REST API, configured to be deployed either as a WAR or a JAR.

---
+++ src/main/java/rest/start/SpringBootTomcatApplication.java
@@ -0,0 +1,8 @@
+package rest.start;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootTomcatApplication extends SpringBootServletInitializer {
+}
